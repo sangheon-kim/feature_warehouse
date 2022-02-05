240988065cdebfab9cc930033e64a50d2f562ae0
sangheon-kim <ksj83671@gmail.com>
2022-02-05 15:47:03 +0900

    [김상헌] Swagger 기본 세팅

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
@@ -7,6 +8,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // class-validation 적용 처리
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle(`Sangheon's API warehouse`)
+    .setDescription('상헌이의 API 창고')
+    .setVersion(`v 0.0.1`)
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // 첫번쨰 인수가 parameter값
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
