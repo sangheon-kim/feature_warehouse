79d3961947e296a4a59d26fd51d2d7c1ed3892f4
sangheon-kim <ksj83671@gmail.com>
2022-02-05 12:31:01 +0900

    [김상헌] cross-env, configuration 세팅 및 prod dev 모드 분기

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development' ? 'envs/.env.dev' : 'envs/.env',
+    }),
     MongooseModule.forRoot(
-      `mongodb+srv://test:1234@cluster0.gqmop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MONGOOSE_USERID}:${process.env.MONGOOSE_PASSWORD}@cluster0.gqmop.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
     ),
   ],
   controllers: [AppController],
