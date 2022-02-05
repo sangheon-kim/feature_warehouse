4209ed687dd29b6ec926bc44b72eb169b7cb8534
sangheon-kim <ksj83671@gmail.com>
2022-02-05 18:44:57 +0900

    [김상헌] 스웨거 보안 설정:

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { CatsModule } from 'src/cats/cats.module';
@@ -7,10 +8,16 @@ import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? '../../envs/.env.dev'
+          : '../../envs/.env',
+    }),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     // 로그인 할 때 사용
     JwtModule.register({
-      secret: process.env.JWT_SECRET_KEY,
+      secret: process.env.JWT_SECRET_KEY || 'secret',
       signOptions: { expiresIn: '1y' },
     }),
 
