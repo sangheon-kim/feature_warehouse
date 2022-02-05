5c896aecf8b49a16dc94225f570750912095bf7c
sangheon-kim <ksj83671@gmail.com>
2022-02-05 13:48:27 +0900

    [김상헌] controller 기본 세팅

--- server/src/cats/cats.controller.ts
+++ server/src/cats/cats.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Get()
+  getAllCat() {
+    return 'get all cat api';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'get one cat api';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat api';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat api';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update partial cat api';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
